28e55c7ac9dbf926ff01644abe0a46cf3d943d97
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-04-19 14:17:55 -0500

    maps thru ingredients

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,17 @@
+import { IngredientsIndex } from "./IngredientsIndex";
 import { PantryItemsIndex } from "./PantryItemsIndex";
 
 export function Content() {
+  const ingredients = [
+    { id: 1, name: "Flour", image_url: "flour.png" },
+    { id: 2, name: "Sugar", image_url: "Sugar.png" },
+    { id: 3, name: "Oil", image_url: "Oil.png" },
+    { id: 4, name: "Eggs", image_url: "Eggs.png" },
+  ];
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <IngredientsIndex ingredients={ingredients} />
       <PantryItemsIndex />
     </main>
   );
