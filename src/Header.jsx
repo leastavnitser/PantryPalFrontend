f1fff302772e913256742ec0e34f4e93d696fc70
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-04-22 11:43:07 -0500

    adds signup link

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,8 +2,8 @@ export function Header() {
   return (
     <header>
       <nav>
-        <a href="#">Home</a> | <a href="#">SignUp</a> |<a href="./login.json">LogIn</a> |<a href="#">MyPantry</a> |{" "}
-        <a href="#">MyRecipes</a>
+        <a href="#">Home</a> | <a href="./signup.json">SignUp</a> |<a href="./login.json">LogIn</a> |
+        <a href="#">MyPantry</a> | <a href="#">MyRecipes</a>
       </nav>
     </header>
   );
