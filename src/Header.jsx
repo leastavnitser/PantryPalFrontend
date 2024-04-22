2125f82deb07bdd0a1b69eceba55dd4f934c6766
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-04-22 11:30:29 -0500

    adds basic nav

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,8 @@ export function Header() {
   return (
     <header>
       <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+        <a href="#">Home</a> | <a href="#">SignUp</a> |<a href="#">LogIn</a> |<a href="#">MyPantry</a> |{" "}
+        <a href="#">MyRecipes</a>
       </nav>
     </header>
   );
