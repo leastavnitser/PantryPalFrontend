2cfa96154784ce1bac3fa121797e3b1e4782d897
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-05-03 12:16:55 -0500

    search and custom ingredient

--- src/IngredientsIndex.jsx
+++ src/IngredientsIndex.jsx
@@ -44,14 +44,50 @@ const Card = ({ ingredient, onShowIngredient }) => {
 // IngredientsIndex component using the Card component
 export function IngredientsIndex(props) {
   const [searchFilter, setSearchFilter] = useState("");
-  console.log(props);
+
+  const filteredIngredients = props.ingredients.filter((ingredient) =>
+    ingredient.name.toLowerCase().includes(searchFilter.toLowerCase())
+  );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onCreateIngredient(params, () => event.target.reset());
+  };
   return (
     <div>
       <h2>Search Ingredients:</h2>
-      <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+      <input
+        type="text"
+        value={searchFilter}
+        onChange={(event) => setSearchFilter(event.target.value)}
+        list="ingredient-suggestions"
+        className="form-control"
+        placeholder="Search for ingredients..."
+      />
+      <datalist id="ingredient-suggestions">
+        {props.ingredients.map((ingredient) => (
+          <option key={ingredient.id} value={ingredient.name} />
+        ))}
+      </datalist>
+      <div>
+        <h1>New Ingredient</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="row g-3">
+            <div className="col">
+              Name: <input name="name" type="text" className="form-control" />
+            </div>
+            <div className="col">
+              Image Url: <input name="image_url" type="text" className="form-control" />
+            </div>
+            <button type="submit" className="btn btn-outline-danger">
+              Create Ingredient
+            </button>
+          </div>
+        </form>
+      </div>
       <h1>All Ingredients</h1>
       <div className="row row-cols-1 row-cols-md-3 g-5">
-        {props.ingredients.map((ingredient) => (
+        {filteredIngredients.map((ingredient) => (
           <div key={ingredient.id} className="col">
             <Card ingredient={ingredient} onShowIngredient={props.onShowIngredient} />
           </div>
