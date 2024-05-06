04c7e7edc5fe16b8639edc1a9f6caf79216de609
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-05-05 22:16:59 -0500

    adds text to home

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,10 +2,17 @@ export function Home() {
   return (
     <div>
       <h1>Welcome to PantryPal!</h1>
-      <h3>The best fridge and pantry system on the market! </h3>
+      <div className="card">
+        <h4>
+          The best fridge and pantry management system on the market! Our groundbreaking technology will be sure to
+          impress you.
+        </h4>
+      </div>
 
       <h3>how it works:</h3>
-      <h4>Easily manage your fridge and pantry by adding ingredients that you have in stock to yourPantry</h4>
+      <div className="card">
+        <h4>Easily manage your fridge and pantry by adding ingredients that you have in stock to yourPantry</h4>
+      </div>
       <h3>whats it for?</h3>
     </div>
   );
