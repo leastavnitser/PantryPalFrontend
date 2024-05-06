868b6e5dbebf6ec4609c43417ee5e93ca7574d22
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-05-05 22:13:54 -0500

    adds img to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,10 +3,17 @@ import "./Footer.css";
 export function Footer() {
   return (
     <footer>
-      <p>Copyright 2024</p>
-      <a href="https://www.linkedin.com/in/lea-stavnitser-879a97285/">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Wb7G67EcR44qT3KQLlLzI1Fna_L2lPXfTI1sx8_z2w&s" />
-      </a>
+      <p>
+        Copyright 2024
+        <div></div>
+        <a href="https://www.linkedin.com/in/lea-stavnitser-879a97285/">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Wb7G67EcR44qT3KQLlLzI1Fna_L2lPXfTI1sx8_z2w&s" />
+        </a>
+        <a href="https://github.com/leastavnitser/PantryPalFrontend">
+          {" "}
+          <img src="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg" />
+        </a>
+      </p>
     </footer>
   );
 }
