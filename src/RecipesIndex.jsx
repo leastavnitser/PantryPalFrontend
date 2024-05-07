a343425f377cc5577faa3f6643fb8cccd6fe111c
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-05-07 16:23:13 -0500

    behold, there is data

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -4,29 +4,36 @@ import axios from "axios";
 export function RecipesIndex() {
   const [allRecipes, setAllRecipes] = useState([]);
 
+  const options = {
+    method: "GET",
+    url: "https://tasty.p.rapidapi.com/recipes/list",
+    params: {
+      from: "0",
+      size: "20",
+      tags: "under_30_minutes",
+    },
+    headers: {
+      "X-RapidAPI-Key": "5dc0cb5a8bmsh42b908dee10f404p11ec10jsn03a933050e52",
+      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+    },
+  };
+  async function fetchData() {
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      setAllRecipes(response);
+    } catch (error) {
+      console.error("Error making API call:", error);
+    }
+  }
 
-const options = {
-  method: 'GET',
-  url: 'https://tasty.p.rapidapi.com/recipes/list',
-  params: {
-    from: '0',
-    size: '20',
-    tags: 'under_30_minutes'
-  },
-  headers: {
-    'X-RapidAPI-Key': '5dc0cb5a8bmsh42b908dee10f404p11ec10jsn03a933050e52',
-    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-  },
-	const response = await axios.request(options);
-	console.log(response.data);
-};
-
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// }
+  fetchData();
+  // try {
+  // 	const response = await axios.request(options);
+  // 	console.log(response.data);
+  // } catch (error) {
+  // 	console.error(error);
+  // }
 
   // const recipes = () => {
   //   console.log("recipes");
@@ -35,6 +42,6 @@ const options = {
   //     setAllRecipes(response.data);
   //   });
   // };
-  useEffect(options, []);
+  // useEffect(options, []);
   return <h1>this is the recipes page.</h1>;
 }
