fe149d224b57f3341c9148cac1cf8dd30e20451c
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-05-08 13:42:24 -0500

    adds gitignore

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -2,46 +2,9 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export function RecipesIndex() {
-  const [allRecipes, setAllRecipes] = useState([]);
-
-  const options = {
-    method: "GET",
-    url: "https://tasty.p.rapidapi.com/recipes/list",
-    params: {
-      from: "0",
-      size: "20",
-      tags: "under_30_minutes",
-    },
-    headers: {
-      "X-RapidAPI-Key": "5dc0cb5a8bmsh42b908dee10f404p11ec10jsn03a933050e52",
-      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-    },
+  const getRecipes = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apikey=${process.env.SPOONACULAR_API_KEY}`);
   };
-  async function fetchData() {
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-      setAllRecipes(response);
-    } catch (error) {
-      console.error("Error making API call:", error);
-    }
-  }
 
-  fetchData();
-  // try {
-  // 	const response = await axios.request(options);
-  // 	console.log(response.data);
-  // } catch (error) {
-  // 	console.error(error);
-  // }
-
-  // const recipes = () => {
-  //   console.log("recipes");
-  //   axios.get("https://tasty.p.rapidapi.com/recipes/list").then((response) => {
-  //     console.log(response.data);
-  //     setAllRecipes(response.data);
-  //   });
-  // };
-  // useEffect(options, []);
   return <h1>this is the recipes page.</h1>;
 }
