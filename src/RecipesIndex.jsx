370bd3d506ad8cf910e49dad1cb19ec80fc4a5bf
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-05-08 14:04:47 -0500

    adds recipe api connection

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -3,7 +3,11 @@ import axios from "axios";
 
 export function RecipesIndex() {
   const getRecipes = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apikey=${process.env.SPOONACULAR_API_KEY}`);
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apikey=${process.env.SPOONACULAR_API_KEY}&number=15`
+    );
+    const data = await api.json();
+    console.log(data);
   };
 
   return <h1>this is the recipes page.</h1>;
