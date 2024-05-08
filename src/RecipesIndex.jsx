9f0bf2a765a8215935a563484b4c9933d4c46095
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-05-08 16:19:04 -0500

    its working but not rendering

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -1,14 +1,34 @@
-import { useState, useEffect } from "react";
+/* eslint-disable no-undef */
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export function RecipesIndex() {
+  const [allRecipes, setAllRecipes] = useState([]);
+  useEffect(() => {
+    getRecipes();
+  }, []);
   const getRecipes = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apikey=${process.env.SPOONACULAR_API_KEY}&number=15`
-    );
-    const data = await api.json();
-    console.log(data);
+    try {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_SPOONACULAR_API_KEY}&number=15`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data);
+      setAllRecipes(data.recipes);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
-  return <h1>this is the recipes page.</h1>;
+  return (
+    <div>
+      <h1>this is the recipes page.</h1>
+      {allRecipes.map((recipe) => {
+        <div key={recipe.id}>{recipe.title}</div>;
+      })}
+    </div>
+  );
 }
