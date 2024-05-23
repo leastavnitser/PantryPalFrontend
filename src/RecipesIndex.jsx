761a9318733f6651616215ff4908c141fa6c7355
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-05-23 10:43:56 -0500

    renders 1 recipe

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+// import {}
+// import axios from "axios";
 export function RecipesIndex() {
   const [allRecipes, setAllRecipes] = useState([]);
   useEffect(() => {
